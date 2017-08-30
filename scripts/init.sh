791e8ce99c375d1d1356b622952a210b6cc326ce
Ken Wenzel <ken.wenzel@iwu.fraunhofer.de>
2017-08-30 16:40:25 +0200

    Add function to initialize .gitconfig from environment variable.

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
-echo "Initialize GIT and SSH keys on host."
+echo "Initialize GIT on host."
 
 # use wrapper script as git command
 if [ -d "/host/usr/bin" ]; then
     cp /scripts/git-wrapper.sh /host/usr/bin/git
     # make git script runnable
     chmod +x /host/usr/bin/git
+fi
+
+GIT_CONFIG_DIR="/host/root"
+# initialize git config from environment variable
+if [ -d "$GIT_CONFIG_DIR" ] && [ ! -f "$GIT_CONFIG_DIR/.gitconfig" ] && [ -n "$GIT_CONFIG_DEFAULT" ] ; then
+	echo "$GIT_CONFIG_DEFAULT" > "$GIT_CONFIG_DIR/.gitconfig"
 fi
